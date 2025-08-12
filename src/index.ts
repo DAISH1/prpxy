4294da8ff1a0613fe7090536d38d5cc458d85a11
DAISH1 <180699981+DAISH1@users.noreply.github.com>
2025-08-12 02:55:15 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { router } from './routes/route';
+import { cacheRoutes } from "./utils/cache-routes";
+
+dotenv.config();
+
+const app = express();
+const PORT = 4040;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(cors({ origin: "*" }));
+app.use(cacheRoutes());
+
+app.use('/', router);
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+
+export default app;
